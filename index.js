5b0ef847a8ba972b8cf68ebfac5abd361198a656
aganyo360 <wilsonaganyo@gmail.com>
2024-07-30 08:55:34 +0300

    updated script file

---
+++ index.js
@@ -0,0 +1,16 @@
+//SvmNeIKp872mseky5 - Public Key
+// service_uona3xr -service id
+// template_b3scibo -template_id
+function contact(event){
+    event.preventDefault()
+    // emailjs
+    //     .sendForm(
+    //         'service_uona3xr',
+    //         'template_b3scibo',
+    //         event.target,
+    //         'SvmNeIKp872mseky5'
+    //     ).then(()=>{
+
+    //         console.log('it workS')
+    //     })
+}
