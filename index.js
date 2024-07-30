010b4fdc27cf378b0b3e93e6ba80dbd5beb4828e
aganyo360 <wilsonaganyo@gmail.com>
2024-07-30 09:30:38 +0300

    modified script

--- index.js
+++ index.js
@@ -13,4 +13,12 @@ function contact(event){
 
     //         console.log('it workS')
     //     })
+    const loading= document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList +=" modal__overlay--visible"
+    setTimeout(()=>{
+        // loading.classList.remove("modal__overlay--visible")
+        // success.classList +="modal__overlay--visible"
+
+    },1000)
 }
