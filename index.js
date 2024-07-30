a4785931ffe7bc7b8c1d2e0f57b84f93f1d3c812
aganyo360 <wilsonaganyo@gmail.com>
2024-07-30 12:22:24 +0300

    updated script file

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-//SvmNeIKp872mseky5 - Public Key
-// service_uona3xr -service id
-// template_b3scibo -template_id
+// Public Key
+// service id
+// template_id
 function contact(event){
-    event.preventDefault()
-    // emailjs
-    //     .sendForm(
-    //         'service_uona3xr',
-    //         'template_b3scibo',
-    //         event.target,
-    //         'SvmNeIKp872mseky5'
-    //     ).then(()=>{
 
-    //         console.log('it workS')
-    //     })
+    event.preventDefault();
     const loading= document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
     loading.classList +=" modal__overlay--visible"
-    setTimeout(()=>{
-        // loading.classList.remove("modal__overlay--visible")
-        // success.classList +="modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_uona3xr',
+            'template_xu0nlua',
+            event.target,
+            'SvmNeIKp872mseky5'
+        ).then(()=>{
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+            
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert("The email service is temporarily unavailable. Please contact me at wilsonganyo@gmail.com")
+        })
+    
+    setTimeout(() => {
+        
 
     },1000)
+}
+
+let isModalOpen= false;
+function ToggleModal(){
+   if(isModalOpen){
+    isModalOpen=false;
+    return document.body.classList.remove('modal--open')
+   }
+   isModalOpen=true;
+    document.body.classList += " modal--open"
 }
